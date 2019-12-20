05ec6629deb4d6ee1785a249d219b3e344ca4d85
Tom Hsu <thomaschsu@gmail.com>
2019-12-20 11:35:22 -0500

    Added data

---
+++ app.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        name: 'John Doe',
+        age: 32,
+        gender: 'male',
+        lookingfor: 'female',
+        location: 'Boston MA',
+        image: 'https://randomuser.me/api/portraits/men/82.jpg'
+    },
+    {
+        name: 'Jen Smith',
+        age: 26,
+        gender: 'female',
+        lookingfor: 'male',
+        location: 'Miami FL',
+        image: 'https://randomuser.me/api/portraits/women/82.jpg'
+    },
+    {
+        name: 'William Johnson',
+        age: 38,
+        gender: 'male',
+        lookingfor: 'female',
+        location: 'Lynn MA',
+        image: 'https://randomuser.me/api/portraits/men/83.jpg'
+    },
+];
